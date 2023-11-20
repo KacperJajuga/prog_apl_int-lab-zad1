269a3acec62a64c7a9a0a61963c0426051d80e9d
Kacper Jajuga <96070925+KacperJajuga@users.noreply.github.com>
2023-11-20 23:09:42 +0100

    create some content in edytuj.html
    display used photo and add file input on edytuj.html

---
+++ produkty/edytuj.js
@@ -0,0 +1,12 @@
+let productsArray = JSON.parse(localStorage.getItem("productsList")) || [];
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("id");
+const i = productsArray.findIndex((obj) => obj.id === parseInt(productId));
+
+if (i === -1) {
+  alert("Produkt nie istnieje.");
+  window.location.href = "./lista.html";
+} else {
+  document.getElementById("title").innerHTML += productsArray[i].name;
+  document.getElementById("image").src = productsArray[i].image;
+}
